c27ef67592b473fd46e3d845e067634e062ba322
Vincent Caux-Brisebois <vcauxbrisebo@nvidia.com>
2026-03-18 22:10:28 +0000

    Fixing license headers to reflect contribution dates

--- gsplat/cuda/_lidar.py
+++ gsplat/cuda/_lidar.py
@@ -1,3 +1,4 @@
+# SPDX-FileCopyrightText: Copyright 2026 the Regents of the University of California, Nerfstudio Team and contributors. All rights reserved.
 # SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from torch import Tensor
 import hashlib
 import math
